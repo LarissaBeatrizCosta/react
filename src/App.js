3be96d5729e5729558469e61da1e0152b198bc14
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-03-05 20:32:29 -0300

    Aprendi CSS no react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Testado Css </h1>
+      <Frase/>
       <HelloWorld/>
+      <SayMyName nome="Larissa"/>
+      <Pessoa nome="Larissa" 
+      idade="17" 
+      profissao="Programadora"/>
+      <List/>
+
     </div>
   );
 }
